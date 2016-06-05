b434cf8ae02d3a91e727e1a4a7f629f7b2d69bb4
Bryan Maass <bryan.maass@gmail.com>
2016-06-05 14:09:41 +1000

    cleanup

--- src/cljs/p_frame/db.cljs
+++ src/cljs/p_frame/db.cljs
@@ -9,23 +9,43 @@
 
 (def app-db (r/atom default-db))
 
+;;;;;;;;;;;;;;;;;;;;;
+;; Register Functions
+
+(defn register-handler [app-frame kw hand-fn]
+  (frame/register-event-handler app-frame kw hand-fn))
+
 (defn regsiter-sub [app-frame kw sub-fn]
   (frame/register-subscription-handler
-   app-frame
-   kw
-   (partial sub-fn app-db)))
+   app-frame kw (partial sub-fn app-db)))
+
+;;;;;;;;;;;;;;;;;;;;;
+;;; Handler Functions
 
 (defn init-db [_ _] default-db)
+
+(defn set-kv [db [_ k v]] (assoc db k v))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Subscription Functions
+
+(defn name-sub [db _] (:name @db))
+
+;;;;;;;;;;;;;;;;;;;;
+;;; Create App Frame
 
 (defn create-app-frame
   ([] (create-app-frame (frame/make-frame nil nil)))
   ([frame]
    (-> frame
-       (frame/register-event-handler :init-db init-db)
-       (frame/register-event-handler :set-kv (fn  [db [_ k v]] (assoc db k v)))
-       (regsiter-sub :name (fn [db _] (:name @db))))))
+       (register-handler :init-db init-db)
+       (register-handler :set-kv set-kv)
+       (regsiter-sub :name name-sub))))
 
 (def app-frame (create-app-frame))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Dispatch and Subscribe
 
 (defn dispatch
   ([dispatch-v] (dispatch app-db dispatch-v))
@@ -35,14 +55,20 @@
 (defn subscribe [subscribe-v]
   (reaction (frame/subscribe app-frame subscribe-v)))
 
+
 (comment
 
   app-db
 
   (dispatch [:init-db])
 
-  (dispatch [:set-kv :name (gensym "lemon")])
-
   @(subscribe [:name])
+
+  ;;; Woo
+  (dotimes [n 1000]
+    (js/setTimeout
+     #(dispatch [:set-kv :name (apply str (shuffle "qwertyuiopasdfghjklzxcvbnm"))])
+     n))
+
 
   )
